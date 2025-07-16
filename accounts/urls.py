d274f79bacb5b8f979ab87798823fe5dc03636f3
harshharaniya <158134844+harshharaniya@users.noreply.github.com>
2025-07-16 11:19:56 +0530

    Fix superuser creation view

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from .views import CustomPasswordResetView
+from .views import create_superuser_view
 
 urlpatterns = [
     # Authentication
